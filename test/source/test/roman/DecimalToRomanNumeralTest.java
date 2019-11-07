94945ca235320f1293f494aa83cd254e0d1d8199
xcesco <xcesco@gmail.com>
2019-11-07 10:53:13 +0100

    initial setp

--- test/source/test/roman/DecimalToRomanNumeralTest.java
+++ test/source/test/roman/DecimalToRomanNumeralTest.java
@@ -4,13 +4,12 @@ package test.roman;
 import org.junit.jupiter.api.Test;
 import dssc.roman.RomanNumeral;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DecimalToRomanNumeralTest {
     @Test
     void zeroIsEmptyString() {
-        RomanNumeral romanNumeral=new RomanNumeral(0);
-        assertEquals("",romanNumeral.toString());
+        RomanNumeral romanNumeral = new RomanNumeral(0);
+        assertEquals("", romanNumeral.toString());
     }
 }
